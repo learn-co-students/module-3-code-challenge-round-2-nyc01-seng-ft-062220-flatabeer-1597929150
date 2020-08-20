0672a31569dfb6d21a379c112ac276e9f4f06bc3
taevonjordan11 <taevonjordan1@gmail.com>
2020-08-20 09:58:50 -0400

    appending dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-// Code here
+
+document.addEventListener("DOMContentLoaded", () => {
+  const PATCHURL ="http://localhost:3000/beers/1"
+
+
+
+  const renderHtml = (beerObj) => {
+    const name = beerObj.name
+    const image = beerObj.image_url
+    const description = beerObj.description
+    const reviews = beerObj.reviews.forEach(review => {
+      
+    })
+
+    const beerNameContainer = document.querySelector("body > main > div > h2").textContent = name
+    const beerImageContainer = document.querySelector("body > main > div > img").src = image
+    const beerDescContainer = document.querySelector("body > main > div > form.description > textarea").textContent = description
+  }
+
+
+
+
+  const getBeer = () => {
+    fetch(PATCHURL)
+    .then(res => res.json())
+    .then(beerObj => renderHtml(beerObj))
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  getBeer()
+
+})

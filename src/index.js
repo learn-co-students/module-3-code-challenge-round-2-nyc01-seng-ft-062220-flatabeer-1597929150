3e0c88505031c367df11b66decd9959f54f729a5
taevonjordan11 <taevonjordan1@gmail.com>
2020-08-20 10:08:49 -0400

    append to dom

--- src/index.js
+++ src/index.js
@@ -9,12 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const image = beerObj.image_url
     const description = beerObj.description
     const reviews = beerObj.reviews.forEach(review => {
-      
+      const li = document.createElement('li')
+      li.append(review)
+      const reviewContainer = document.querySelector("body > main > div > ul").append(li)
     })
+
 
     const beerNameContainer = document.querySelector("body > main > div > h2").textContent = name
     const beerImageContainer = document.querySelector("body > main > div > img").src = image
     const beerDescContainer = document.querySelector("body > main > div > form.description > textarea").textContent = description
+
   }
 
 

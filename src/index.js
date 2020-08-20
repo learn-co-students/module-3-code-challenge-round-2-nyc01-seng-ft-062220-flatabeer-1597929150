3607362e4dddfb16b284c46b5331c778b045e874
vakas-786 <vakas0959@gmail.com>
2020-08-20 09:58:25 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-// Code here
+document.addEventListener('DOMContentLoaded', () => {
+    
+    fetch('http://localhost:3000/beers/1')
+    .then(response => response.json())
+    .then(beers => renderBeers(beers))
+
+    const renderBeers = (beers) => {
+        const name = document.querySelector("body > main > div > h2")
+        name.innerText = beers.name 
+        const image = document.querySelector("body > main > div > img")
+        image.src = beers.image_url
+        const description = document.querySelector("body > main > div > form.description > textarea")
+        description.innerText = beers.description
+        
+        beers.reviews.forEach(review => {
+            const reviewUl = document.querySelector("body > main > div > ul")
+            const reviewLi = document.createElement('li')
+            reviewLi.innerText = review
+            reviewUl.appendChild(reviewLi)
+        })
+    }
+
+
+})

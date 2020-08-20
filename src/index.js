d575d1388b687f2fe0eb18934a9694498bb1e156
yulia <juliana.ny2008@gmail.com>
2020-08-20 09:58:46 -0400

    deliverable 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-// Code here
+// See the first beer's details, including its name, image, description, and reviews, when the page loads
+// Change the beer's description and still see that change when reloading the page
+// Add a review for the beer (no persistence needed)
+
+document.addEventListener("DOMContentLoaded", function() { 
+
+const name = document.querySelector('h2')
+const img = document.querySelector('img')
+
+
+    function renderBeer(){
+        fetch( "http://localhost:3000/beers/1")
+        .then(resp => resp.json())
+        .then(obj =>)
+         }
+       }
+
+
+
+
+
+
+
+
+
+}
